ffe1b84fa621ffede5ddc5d88588ab2c45607d69
Caleb Canada <calebcanada13@gmail.com>
2018-07-27 03:59:48 -0500

    Check in

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Movies Shottas = new Movies("Shottas", 3, 2002);
+        Shottas.printMovie();
+
+        Movies bebesKids = new Movies("Bebe's Kids", 4, 1992);
+        bebesKids.printMovie();
+
+    }
+}
---
+++ src/MainProduct.java
@@ -0,0 +1,6 @@
+public class MainProduct {
+    public static void main(String[] args) {
+        Product store = new Product("Banana", 1.1, 13);
+        store.printProduct();
+    }
+}
